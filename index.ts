5818c38092f72f1f03a43bc9b7a8697725e5b771
Conrad Petrich <conrad.p@purple.com>
2021-05-12 22:53:04 -0600

    Script runs on a cron job every 10 seconds to update noip domains to use current public facing ip of machine running script.

--- index.ts
+++
@@ -1,5 +0,0 @@
-function main() {
-    console.log(process.env.NOIP_USERNAME, process.env.NOIP_PASSWORD);
-}
-
-main();
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { schedule } from "node-cron";
+
+async function updateIp() {
+  const isSuccessful = (response: string): boolean =>
+    ["nochg", "good"].includes(response.split(" ")[0]);
+
+  try {
+    const ip_response = await axios
+      .get(`http://ip1.dynupdate.no-ip.com/`, {
+        validateStatus: (status: number) => status >= 200 && status < 300,
+      })
+      .catch((error: AxiosError) => {
+        throw error;
+      });
+    const ip: string = ip_response.data;
+
+    const update_response = await axios
+      .get(
+        `https://${process.env.NOIP_USERNAME}:${process.env.NOIP_PASSWORD}@dynupdate.no-ip.com/nic/update?hostname=${process.env.DOMAINS}&myip=${ip}`,
+        {
+          validateStatus: (status: number) => status >= 200 && status < 300,
+        }
+      )
+      .catch((error: AxiosError) => {
+        throw error;
+      });
+    if (!isSuccessful(update_response.data)) {
+      throw update_response.data;
+    }
+    console.log(update_response.data);
+  } catch (error: any) {
+    throw error;
+  }
+}
+
+// This is a cron job for updating your domains to have the correct public facing ip of the network the machine is currently running on
+// Scheduled to run every 4 hours by default
+schedule("*/10 * * * * *", async () => {
+  try {
+    await updateIp();
+  } catch (error: any) {
+    console.log(error);
+  }
+});
